b9f90aedd06039257529acc28ce971ca96ab91a0
Andrew <andrew@almostimplemented.com>
2016-04-04 00:35:22 -0700

    i meant hour --> minute .. had that problem in Main too lol

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -28,29 +28,33 @@ case class Edge(v: String, w: String, timestamp: Long) {
       (w,v).hashCode()
   }
 }
+
 class GraphState {
   // Constant for checking expiration
-  val ONE_HOUR: Long = 3600000 // milliseconds
+  val ONE_MINUTE: Long = 60000 // milliseconds
 
   // Mutable State
   private var mostRecentTimestamp: Long = -1
-  val edges = mutable.ListBuffer[Edge]()
-  val nodes = mutable.ListBuffer[Node]()
+  private val edges = mutable.ListBuffer[Edge]()
+  private val nodes = mutable.ListBuffer[Node]()
+
+  // Fast lookup for existing nodes and edges
+  val hashtagMap = mutable.HashMap[String,Node]()
+  val edgeSet  = mutable.HashSet[Edge]()
+
+  // Querying the graph
   def edgeCount = edges.length
   def nodeCount = nodes.length
   def averageDegree = {
-    if (nodes.isEmpty) 0.0 else (2.0 * edgeCount)/nodeCount
+    if (nodes.isEmpty) 0.0 else (2.0 * edgeCount) / nodeCount
   }
-  // fast lookup for existing nodes + nodeCount
-  val hashtagMap = mutable.HashMap[String,Node]()
-  val edgeSet  = mutable.HashSet[Edge]()
-  // Accounting methods
 
+  // Accounting methods
   def auditEdges(): Unit = {
     if (edges.length < 1)
       return
     val e = edges.head
-    if (mostRecentTimestamp - e.timestamp < ONE_HOUR) {
+    if (mostRecentTimestamp - e.timestamp < ONE_MINUTE) {
       return
     } else {
       edges -= e
@@ -63,7 +67,7 @@ class GraphState {
     if (nodes.length < 1)
       return
     val n = nodes.head
-    if (mostRecentTimestamp - n.timestamp < ONE_HOUR) {
+    if (mostRecentTimestamp - n.timestamp < ONE_MINUTE) {
       return
     } else {
       nodes -= n
@@ -80,22 +84,41 @@ class GraphState {
     }
   }
 
-  def processHashtags(hashtags: Set[String], epoch: Long) = {
+  // These two methods are for when we get something out of order
+  // It is called if the received tweet is not the most recent tweet
+  def insertNodesCarefully(l: mutable.ListBuffer[Node], newNodes: mutable.ListBuffer[Node]) = {
+    val timestamp = newNodes(0).timestamp
+    val index = (l.length - 1) - l.reverseIterator.indexWhere {
+      (n: Node) => n.timestamp < timestamp
+    }
+    l.insert(index, newNodes:_*)
+  }
+  def insertEdgesCarefully(l: mutable.ListBuffer[Edge], newEdges: mutable.ListBuffer[Edge]) = {
+    val timestamp = newEdges(0).timestamp
+    val index = (l.length - 1) - l.reverseIterator.indexWhere {
+      (e: Edge) => e.timestamp < timestamp
+    }
+    l.insert(index, newEdges:_*)
+  }
+
+  def processHashtags(hashtags: Set[String], epoch: Long) : Unit = {
     // First, remove any expired graph elements
     audit(epoch)
-    // Only modify the graph when necessary
-    if (mostRecentTimestamp - epoch < ONE_HOUR && hashtags.size > 1) {
-      // Next, add new nodes and update timestamps
+
+    if (hashtags.size < 2) return
+
+    // First deal with "in order" tweets
+    if (mostRecentTimestamp == epoch) {
       for (s <- hashtags) {
         val n: Node = Node(s, epoch)
         if (hashtagMap contains s) {
           val previous: Node = hashtagMap(s)
           nodes -= previous
           nodes.append(n)
-          hashtagMap.update(s, n)
+          hashtagMap += (s -> n)
         } else {
-          hashtagMap.put(s, n)
           nodes.append(n)
+          hashtagMap += (s -> n)
         }
       }
       // Finally, add new edges and update timestamps
@@ -106,13 +129,51 @@ class GraphState {
           case Some(previous: Edge) => {
             edges -= previous
             edges.append(e)
+            edgeSet += e
           }
           case None => {
-            edgeSet.add(e)
             edges.append(e)
+            edgeSet += e
           }
         }
       }
+    } else if (mostRecentTimestamp - epoch < ONE_MINUTE) {
+      // This one is out of order and needs special insertion
+      val newNodes = mutable.ListBuffer[Node]()
+      val newEdges = mutable.ListBuffer[Edge]()
+      for (s <- hashtags) {
+        val n: Node = Node(s, epoch)
+        if (hashtagMap contains s) {
+          val previous: Node = hashtagMap(s)
+          if (previous.timestamp < epoch) {
+            nodes -= previous
+            newNodes.append(n)
+            hashtagMap.update(s, n)
+          }
+        } else {
+          hashtagMap.put(s, n)
+          newNodes.append(n)
+        }
+      }
+      val pairs = hashtags.toVector combinations 2
+      for (pair <- pairs) {
+        val e: Edge = Edge(pair(0), pair(1), epoch)
+        edgeSet find (_ == e) match {
+          case Some(previous: Edge) => {
+            if (previous.timestamp < epoch) {
+              edges -= previous
+              newEdges.append(e)
+              edgeSet += e
+            }
+          }
+          case None => {
+            edgeSet += (e)
+            newEdges.append(e)
+          }
+        }
+      }
+      insertNodesCarefully(nodes, newNodes)
+      insertEdgesCarefully(edges, newEdges)
     }
   }
 }
@@ -141,9 +202,9 @@ object Main extends App {
       // Only process lines that are tweets
       if ((has(obj, "entities"))) {
         // First we extract the timestamp
-        val JString(epochString) = obj \ "timestamp_ms"
-        val epoch: Long = epochString.toLong
-
+        val JString(createdAt) = obj \ "created_at"
+        val d = format.parse(createdAt)
+        val epoch: Long = d.getTime
         // Next we extract the hashtags, removing duplicate strings
         val JArray(hashtagArray) = obj \ "entities" \ "hashtags"
         val hashtags: Set[String] = (for {
@@ -161,5 +222,4 @@ object Main extends App {
     input.close()
   }
   val t1 = System.nanoTime()
-  println("Elapsed time: " + (t1 - t0) + "ns")
 }
